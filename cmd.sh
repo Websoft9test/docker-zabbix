7568f00cc64dd3ffc9a801cba5928d6ce1f18a1e
qiaofeng1227 <76487013@qq.com>
2022-05-31 09:21:46 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,6 @@
 while true
 do
-    field_lines=`mysql -uroot -p$new_password -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  surname='Administrator';"`
+    field_lines=`mysql -uroot -p$APP_PASSWORD -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  surname='Administrator';"`
     sleep 3
     if [ $field_lines -ge  1 ];then
         break
@@ -9,8 +9,8 @@ done
 
 while true
 do
-    password_lines=`mysql -uroot -p$new_password -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  length(passwd) < 60;"`
-    sudo mysql -uroot -p"$new_password" -h 127.0.0.1 -e "update zabbix.users set passwd=md5('$new_password') where surname='Administrator';"
+    password_lines=`mysql -uroot -p$APP_PASSWORD -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  length(passwd) < 60;"`
+    sudo mysql -uroot -p"$APP_PASSWORD" -h 127.0.0.1 -e "update zabbix.users set passwd=md5('$APP_PASSWORD') where surname='Administrator';"
     sleep 3
     if [ $password_lines -ge  1 ];then
        break
