36e6468ff7ecdd1bc31dc58a38fe1852d3902595
qiaofeng1227 <76487013@qq.com>
2022-06-10 10:43:03 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -6,4 +6,5 @@ app_pass=$(htpasswd -bnBC 10 "" $APP_PASSWORD | tr -d ':')
 echo $app_pass
 #sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" /data.tmpl
 
-/usr/bin/docker-entrypoint.sh
+/usr/bin/tini -- /usr/bin/docker-entrypoint.sh
+
