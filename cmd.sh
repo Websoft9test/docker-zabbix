1fac9f4da164373867ce460cd48587a2476ddfd2
qiaofeng1227 <76487013@qq.com>
2022-06-10 11:10:03 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,9 @@ echo "##########################################start set init password#########
 
 app_pass=$(htpasswd -bnBC 10 "" $APP_PASSWORD | tr -d ':')
 echo $app_pass
-#sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" /data.tmpl
 
+cd /usr/share/doc/zabbix-server-mysql
+gunzip create.sql.gz
+#sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" create.sql
+gzip create.sql
 /usr/bin/tini -- /usr/bin/docker-entrypoint.sh
-
