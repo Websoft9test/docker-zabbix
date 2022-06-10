5e7d4320a2e9d77f42e4f3a406027da57d3e6d3a
qiaofeng1227 <76487013@qq.com>
2022-06-10 14:04:53 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -8,6 +8,8 @@ echo "##########################################start set init password#########
 cd /usr/share/doc/zabbix-server-mysql
 gunzip create.sql.gz
 #sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" create.sql
+chmod +x create.sql
 gzip create.sql
+
 #/usr/bin/tini -- /usr/bin/docker-entrypoint.sh
 /usr/bin/docker-entrypoint.sh
