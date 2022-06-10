bb261c728c88e9a17d28562c3ba42978fc74bdb0
qiaofeng1227 <76487013@qq.com>
2022-06-10 09:03:22 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,6 @@ echo "##########################################start set init password#########
 
 app_pass=$(htpasswd -bnBC 10 "" $APP_PASSWORD | tr -d ':')
 echo $app_pass
-sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" ?
+sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" /data.tmpl
 
 /usr/bin/docker-entrypoint.sh
