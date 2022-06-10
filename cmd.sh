a43d515d3792115fed0229117fdbe40e93a3d49b
qiaofeng1227 <76487013@qq.com>
2022-06-10 15:22:59 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
 echo "##########################################start set init password#################################################################"
 
-#app_pass=$(htpasswd -bnBC 10 "" $APP_PASSWORD | tr -d ':')
-#echo $app_pass
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+yum update -y
+yum install httpd -y
+
+app_pass=$(htpasswd -bnBC 10 "" $APP_PASSWORD | tr -d ':')
+echo $app_pass
 
 cd /usr/share/doc/zabbix-server-mysql
 gunzip create.sql.gz
-sed -i "s/\$2y\$10\$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/\$2y\$10\$t7MB0k5ac5gALk9cPObSuuvtoKK0wTqqT07yjXb5WlOhZUqQcY0hy/g" create.sql
+sed -i "s/\$2y\$10\$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" create.sql
 gzip create.sql
 /usr/bin/tini -- /usr/bin/docker-entrypoint.sh /usr/sbin/zabbix_server --foreground -c/etc/zabbix/zabbix_server.conf
