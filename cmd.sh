75f7acfb2b39c7bc7f9ca42d1a5e8fb20eaa51c1
qiaofeng1227 <76487013@qq.com>
2022-06-10 11:37:11 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -9,4 +9,5 @@ cd /usr/share/doc/zabbix-server-mysql
 gunzip create.sql.gz
 #sed -i "s/$2y$10$92nDno4n0Zm7Ej7Jfsz8WukBfgSS\/U0QkIuu8WkJPihXBb2A1UrEK/$app_pass/g" create.sql
 gzip create.sql
-/usr/bin/tini -- /usr/bin/docker-entrypoint.sh
+#/usr/bin/tini -- /usr/bin/docker-entrypoint.sh
+/usr/bin/docker-entrypoint.sh
