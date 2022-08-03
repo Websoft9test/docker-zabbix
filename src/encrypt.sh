3e99e094b881094a7ad739e9ee50a9664dcc36bc
qiaofeng1227 <76487013@qq.com>
2022-08-03 10:52:42 +0800

    Update encrypt.sh

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -6,4 +6,6 @@ apt-get install apache2-utils -y 1>/dev/null 2>&1
 yum install httpd-tools -y 1>/dev/null 2>&1
 
 app_pass=$(htpasswd -bnBC 10 "" $1 | tr -d ':')
+app_pass=$(echo $app_pass |sed 's/\$/\\$/g')
+app_pass=$(echo $app_pass |sed 's/\//\\\//g')
 echo $app_pass
